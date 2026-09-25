cd984900fa9982a464c89fbc634255cb5ddaa899
RansomBroker <yadisoke@gmail.com>
2026-09-25 11:04:40 +0700

    refactor: remove FilamentInfoWidget from Backoffice panel widgets

--- public/diego-music-store/app/Providers/Filament/BackofficePanelProvider.php
+++ public/diego-music-store/app/Providers/Filament/BackofficePanelProvider.php
@@ -14,7 +14,6 @@ use Filament\Support\Colors\Color;
 use Filament\Support\Facades\FilamentView;
 use Filament\View\PanelsRenderHook;
 use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -95,7 +94,6 @@ class BackofficePanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
                 AccountWidget::class,
-                FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
